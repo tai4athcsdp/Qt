8aa5959d4100fc4c5ecc8ce3043a671bdd35d389
taiemotiv <tai@emotiv.com>
2026-04-18 11:37:45 +0700

    init library project

--- InfomationManagementApp/MyLibrary/main.cpp
+++ InfomationManagementApp/MyLibrary/main.cpp
@@ -4,15 +4,15 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("MyLibrary", "Main");
+    engine.loadFromModule("LibraryApp", "Main");
 
     return app.exec();
 }
